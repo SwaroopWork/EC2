11a81587584dd695ae465e9246c054d2cb4051f4
intimetecbci <kori.swaroopwork@gmail.com>
2025-04-11 13:31:06 +0530

    initial commit

--- lib/infra-stack.ts
+++ lib/infra-stack.ts
@@ -1,5 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import { readFileSync } from 'fs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class InfraStack extends cdk.Stack {
@@ -12,5 +14,31 @@ export class InfraStack extends cdk.Stack {
     // const queue = new sqs.Queue(this, 'InfraQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
+    const demoVPC = new ec2.Vpc(this, 'demoVPC', {
+      vpcName:'demoVPC',
+      ipAddresses:ec2.IpAddresses.cidr('10.0.0.0/16'),
+      natGateways:0,
+    });
+        //Security Group
+    const demoSG = new ec2.SecurityGroup(this,'demoSG',{
+      vpc:demoVPC,
+      securityGroupName:'allow http traffic',
+      allowAllOutbound:true,
+    });
+    demoSG.addIngressRule(ec2.Peer.anyIpv4(),ec2.Port.tcp(80),'allow http traffic')
+
+       //EC2 Instance
+       const demoEC2 = new ec2.Instance(this,'demoEC2',{
+        vpc:demoVPC,
+        vpcSubnets:{subnetType:ec2.SubnetType.PUBLIC},
+        securityGroup:demoSG,
+        instanceType:ec2.InstanceType.of(ec2.InstanceClass.T2,ec2.InstanceSize.MICRO),
+        machineImage: ec2.MachineImage.latestAmazonLinux2023(),
+        keyName:'demo',})
+
+        const userData=readFileSync('C:/Users/swaroop.kori/ec2/infra/lib/userdata.sh', 'utf8');
+        
+
+        demoEC2.addUserData(userData);
   }
 }
